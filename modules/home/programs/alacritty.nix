c0832e375e236565fdeb46d21d10fc40e9450a4b
Terrame0 <terrame_0@proton.me>
2026-01-04 04:54:44 +1000

    [gen 81 | ver 25.11.20251228.9ef2612 | 2026-01-04 04:54:42]

---
+++ modules/home/programs/alacritty.nix
@@ -0,0 +1,38 @@
+{...}: {
+  programs.alacritty = {
+    enable = true;
+    settings = {
+      window = {
+        padding = {
+          x = 8;
+          y = 8;
+        };
+        dynamic_padding = true;
+        decorations = "None";
+      };
+
+      font = {
+        normal = {
+          family = "JetBrainsMono Nerd Font";
+          style = "Regular";
+        };
+        bold = {
+          family = "JetBrainsMono Nerd Font";
+          style = "Bold";
+        };
+        italic = {
+          family = "JetBrainsMono Nerd Font";
+          style = "Italic";
+        };
+        size = 14.0;
+      };
+
+      cursor = {
+        style = {
+          shape = "Beam";
+          blinking = "Off";
+        };
+      };
+    };
+  };
+}
--- modules/system/fonts.nix
+++ modules/system/fonts.nix
@@ -1,13 +1,9 @@
 {pkgs, ...}: {
+  fonts.fontconfig.enable = true;
   fonts.packages = with pkgs; [
+    nerd-fonts.jetbrains-mono
     noto-fonts
     noto-fonts-cjk-sans
     noto-fonts-color-emoji
-    liberation_ttf
-    fira-code
-    fira-code-symbols
-    mplus-outline-fonts.githubRelease
-    dina-font
-    proggyfonts
   ];
 }
