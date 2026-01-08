2a1a552329ed9b0b76241e2f90029f80a4cf18dd
Terrame0 <terrame_0@proton.me>
2026-01-08 23:34:51 +1000

    [gen 93 | ver 25.11.20251228.9ef2612 | 2026-01-08 23:34:49]

---
+++ modules/home/gnome-configuration/idle.nix
@@ -0,0 +1,11 @@
+{...}: {
+  dconf.settings = {
+    "org/gnome/desktop/session" = {
+      idle-delay = 0;
+    };
+
+    "org/gnome/desktop/screensaver" = {
+      lock-delay = 0;
+    };
+  };
+}
