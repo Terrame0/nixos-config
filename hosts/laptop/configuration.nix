1dfda2409952e6f707a85b295f764591d077d958
Terrame0 <terrame_0@proton.me>
2026-01-08 23:38:36 +1000

    [gen 94 | ver 25.11.20251228.9ef2612 | 2026-01-08 23:38:34]

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -30,6 +30,7 @@
   # -- networking
   networking.hostName = "laptop";
   networking.networkmanager.enable = true;
+  networking.enableIPv6 = false;
 
   # -- time and locale
   time.timeZone = "Asia/Vladivostok";
