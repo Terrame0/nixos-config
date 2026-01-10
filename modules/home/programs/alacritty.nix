bde3449e20c183914ad8d8223f88f1d0a046e763
Terrame0 <terrame_0@proton.me>
2026-01-10 13:15:35 +1000

    [gen 115 | ver 25.11.20260107.d351d06 | 2026-01-10 13:15:33]

--- modules/home/programs/alacritty.nix
+++ modules/home/programs/alacritty.nix
@@ -9,7 +9,26 @@
           y = 8;
         };
         dynamic_padding = true;
+        opacity = 0.7;
+        blur = true;
+        startup_mode = "Maximized";
+        title = "Terminal";
       };
+
+      bindings = [
+        {
+          key = "C";
+          mods = "Control|Shift";
+          mode = "Normal";
+          action = "Copy";
+        }
+        {
+          key = "V";
+          mods = "Control|Shift";
+          mode = "Normal";
+          action = "Paste";
+        }
+      ];
 
       font = {
         normal = {
