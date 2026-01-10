a8483ecb90ab81b385f2d44b31b3d2bcb54db5bc
Terrame0 <terrame_0@proton.me>
2026-01-10 19:03:07 +1000

    [gen 122 | ver 25.11.20260107.d351d06 | 2026-01-10 19:03:05]

--- modules/system/services/zapret.nix
+++ modules/system/services/zapret.nix
@@ -1,45 +1,58 @@
 {...}: {
   services.zapret = {
     enable = true;
-    configureFirewall = true;
-    httpSupport = true;
-    udpSupport = true;
-    udpPorts = ["50000:60000"]; # -- for voice channels
 
-    # -- dpi bypass strategies
     params = [
-      "--filter-tcp=80 --dpi-desync=fake,multisplit --dpi-desync-split-pos=method+2 --dpi-desync-fooling=md5sig --new"
-      "--filter-tcp=443 --dpi-desync=fake,multidisorder --dpi-desync-split-pos=1,midsld --dpi-desync-fooling=badseq,md5sig --new"
+      # -- http
+      "--filter-tcp=80 <HOSTLIST>"
+      "--dpi-desync=fake,fakedsplit"
+      "--dpi-desync-autottl=2"
+      "--dpi-desync-fooling=md5sig"
+      "--new"
+
+      # -- https
+      "--filter-tcp=443 <HOSTLIST>"
+      "--dpi-desync=fake,multidisorder"
+      "--dpi-desync-fooling=badseq"
+      "--dpi-desync-split-pos=midsld"
+      "--dpi-desync-fake-tls=0x00000000"
     ];
-    # -- bypass whitelist
+
     whitelist = [
+      # -- discord
+      "discord.com"
+      "app.discord.com"
+      "discordapp.com"
+      "discordcdn.com"
+
       # -- youtube
       "youtube.com"
       "youtu.be"
-      "youtube-nocookie.com"
       "ytimg.com"
       "googlevideo.com"
-      # -- discord
-      "discord.com"
-      "discordapp.com"
-      "discord.gg"
-      "discordcdn.com"
-      "cdn.discordapp.com"
+      "gvt1.com"
+      "ggpht.com"
+      "youtubei.googleapis.com"
+      "youtube.googleapis.com"
+      "youtube-nocookie.com"
+
       # -- steam
       "store.steampowered.com"
       "steamcommunity.com"
-      "steampowered.com"
-      "steamcdn.com"
-      "valvecdn.com"
-      "steamgames.com"
+      "steamusercontent.com"
+      "steamcdn-a.akamaihd.net"
       "steamstatic.com"
+      "steampowered.com"
+      "api.steampowered.com"
+
       # -- modrinth
+      "modrinth.com"
       "api.modrinth.com"
       "cdn.modrinth.com"
-      "images.modrinth.com"
+
       # -- curseforge
+      "curseforge.com"
       "api.curseforge.com"
-      "minecraft.curseforge.com"
       "mediafiles.forgecdn.net"
       "edge.forgecdn.net"
     ];
