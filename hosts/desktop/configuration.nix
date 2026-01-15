4967a37856aefb60d2bba6cea97a7b8a4760f468
Terrame0 <terrame_0@proton.me>
2026-01-16 02:16:07 +1000

    something broke

--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -29,6 +29,18 @@
 
   # -- time and locale
   time.timeZone = "Asia/Vladivostok";
+  i18n.defaultLocale = "en_US.UTF-8";
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "ru_RU.UTF-8";
+    LC_IDENTIFICATION = "ru_RU.UTF-8";
+    LC_MEASUREMENT = "ru_RU.UTF-8";
+    LC_MONETARY = "ru_RU.UTF-8";
+    LC_NAME = "ru_RU.UTF-8";
+    LC_NUMERIC = "ru_RU.UTF-8";
+    LC_PAPER = "ru_RU.UTF-8";
+    LC_TELEPHONE = "ru_RU.UTF-8";
+    LC_TIME = "ru_RU.UTF-8";
+  };
 
   # -- user configuration
   programs.zsh.enable = true;
