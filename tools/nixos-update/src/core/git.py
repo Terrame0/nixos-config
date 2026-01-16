4cbf520c518cab031bf1f47da2234a09180bcb97
Terrame0 <terrame_0@proton.me>
2026-01-16 17:36:43 +1000

    [hostname: desktop] -m [gen: 24 | ver: 25.11.20260113.2c3e5ec] -m [2026-01-16 17:36:40]

--- tools/nixos-update/src/core/git.py
+++ tools/nixos-update/src/core/git.py
@@ -67,12 +67,12 @@ def push_changes():
         f"[gen: {gen_number} | ver: {gen_version}]",
         f"[{gen_date} {gen_time}]",
     ]
-    commit_message=" -m ".join(commit_message_lines) # -- forms the commit message
+    commit_message=" ".join(['-m ' + line for line in commit_message_lines]) # -- forms the commit message
     run("git add .")
     print(f"\033[36m🖹 creating commit: {commit_message}\033[0m")
     for line in commit_message_lines:
         print('  '+line)
-    run(f"git commit -m '{commit_message}'",nofail=True)
+    run(f"git commit {commit_message}",nofail=True)
     print("\033[36m⇈ pushing to remote...\033[0m")
     run(f"git push {script_args().remote} {script_args().branch}")
     print("\033[32m✔ pushed successfuly\033[0m")
