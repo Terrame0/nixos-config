efb03bd6357ede74c94a837add2f33d5e822815c
Terrame0 <terrame_0@proton.me>
2026-01-17 23:27:23 +1000

    [hostname 'desktop']
    [gen 63 | ver 25.11.20260113.2c3e5ec]
    
    [2026-01-17 23:27:21]

--- modules/system/programs/discordo.nix
+++ modules/system/programs/discordo.nix
@@ -1,20 +1,18 @@
-{
-  pkgs,
-  nixos-update-script,
-  ...
-}: let
-  unstable = import (builtins.fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/nixos-unstable.tar.gz";
-  }) {};
+{pkgs, ...}: let
+  unstable =
+    import (builtins.fetchTarball {
+      url = "https://github.com/NixOS/nixpkgs/archive/nixos-unstable.tar.gz";
+      sha256 = "09qwbfkvk3jmqlpgis4v1m1fmh7zffi8drybb03mjqa95bynbs99";
+    }) {
+      system = pkgs.stdenv.hostPlatform.system;
+      config = {};
+    };
   myOverlay = self: super: {
     discordo = unstable.discordo;
   };
 in {
   nixpkgs.overlays = [myOverlay];
   environment.systemPackages = with pkgs; [
-    keyd
-    nix-ld
-    pacproxy
-    nixos-update-script.packages.${pkgs.system}.default
+    discordo
   ];
 }
