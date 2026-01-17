0e06ac7cf1146122a27b089ae632dc132a33edad
Terrame0 <terrame_0@proton.me>
2026-01-17 17:01:30 +1000

    [hostname 'desktop']
    [gen 39 | ver 25.11.20260113.2c3e5ec]
    
    [2026-01-17 17:01:29]

--- modules/home/programs/firefox.nix
+++ modules/home/programs/firefox.nix
@@ -16,6 +16,96 @@
           "network.proxy.ssl" = "127.0.0.1";
           "network.proxy.ssl_port" = 10808;
           "network.proxy.no_proxies_on" = ".deepseek.com,.youtube.com,.vk.com,.yandex.ru,.duckduckgo.com,.google.com";
+
+          # -- startup behaviour
+          "browser.startup.homepage" = "about:blank";
+          "browser.startup.page" = 3; # -- auto restore session
+
+          # -- session restore
+          "browser.sessionstore.resume_from_crash" = true;
+          "browser.sessionstore.resume_session_once" = false;
+
+          # -- search defaults
+          "browser.search.defaultenginename" = "ddg";
+          "browser.search.order.1" = "ddg";
+        };
+        search = {
+          force = true;
+          default = "ddg";
+          order = [
+            "ddg"
+            "google"
+            "NixOS Packages"
+            "NixOS Options"
+            "Nix Flakes"
+            "Home Manager Options"
+          ];
+
+          engines = {
+            "NixOS Packages" = {
+              urls = [
+                {
+                  template = "https://search.nixos.org/packages";
+                  params = [
+                    {
+                      name = "query";
+                      value = "{searchTerms}";
+                    }
+                  ];
+                }
+              ];
+              icon = "https://nixos.org/favicon.ico";
+              definedAliases = ["@np"];
+            };
+
+            "NixOS Options" = {
+              urls = [
+                {
+                  template = "https://search.nixos.org/options";
+                  params = [
+                    {
+                      name = "query";
+                      value = "{searchTerms}";
+                    }
+                  ];
+                }
+              ];
+              icon = "https://nixos.org/favicon.ico";
+              definedAliases = ["@no"];
+            };
+
+            "Home Manager Options" = {
+              urls = [
+                {
+                  template = "https://home-manager-options.extranix.com/";
+                  params = [
+                    {
+                      name = "query";
+                      value = "{searchTerms}";
+                    }
+                  ];
+                }
+              ];
+              icon = "https://nixos.org/favicon.ico";
+              definedAliases = ["@hm"];
+            };
+
+            "Nix Flakes" = {
+              urls = [
+                {
+                  template = "https://github.com/search";
+                  params = [
+                    {
+                      name = "q";
+                      value = "{searchTerms} flake.nix";
+                    }
+                  ];
+                }
+              ];
+              icon = "https://github.com/favicon.ico";
+              definedAliases = ["@nf"];
+            };
+          };
         };
       };
     };
