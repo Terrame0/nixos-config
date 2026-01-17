e4f6e7624b80265e9d106322a0601c7372d37c0a
Terrame0 <terrame_0@proton.me>
2026-01-17 22:04:34 +1000

    [hostname 'desktop']
    [gen 57 | ver 25.11.20260113.2c3e5ec]
    
    [2026-01-17 22:04:32]

--- modules/system/nix-pm.nix
+++ modules/system/nix-pm.nix
@@ -1,8 +1,8 @@
 {...}: {
   nixpkgs.config.allowUnfree = true;
-  # nix.settings = {
-  #   experimental-features = ["nix-command" "flakes"];
-  #   http-proxy = "http://127.0.0.1:8080";
-  #   https-proxy = "http://127.0.0.1:8080";
-  # };
+  nix.settings = {
+    "experimental-features" = ["nix-command" "flakes"];
+    # "http-proxy" = "http://127.0.0.1:8080";
+    # "https-proxy" = "http://127.0.0.1:8080";
+  };
 }
