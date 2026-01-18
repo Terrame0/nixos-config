5d3d295b2a86666d66186370ace8a9e1481c2b10
Terrame0 <terrame_0@proton.me>
2026-01-18 20:07:10 +1000

    [WIP]
    [hostname 'desktop']
    
    [gen 78 | ver 25.11.20260113.2c3e5ec]
    
    [2026-01-18 20:01:40]

--- tools/nixos-update/src/core/rebuild.py
+++ tools/nixos-update/src/core/rebuild.py
@@ -5,5 +5,5 @@ def rebuild():
     run("alejandra .")
     run("git add .")
     print("\033[36m↻ rebuilding...\033[0m")
-    run(f"sudo nixos-rebuild switch --flake {script_args().config_path}#{run("hostname").stdout.strip('\n') if script_args().hostname == "current" else script_args().hostname} --print-build-logs --verbose --log-format internal-json |& nom --json", print_output=True)
+    run(f"sudo nixos-rebuild switch --flake {script_args().config_path}#{script_args().hostname} --print-build-logs --verbose --log-format internal-json |& nom --json", print_output=True)
     print("\033[32m✔ rebuilt successfuly\033[0m")
