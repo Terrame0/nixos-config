ad8de9a2f4b5c6a73ada42bfef5d2d14342f550c
Terrame0 <terrame_0@proton.me>
2026-01-19 01:36:11 +1000

    [WIP]
    [hostname 'desktop']
    
    [gen 89 | ver 25.11.20260113.2c3e5ec]
    
    [2026-01-19 01:31:37]

--- modules/home/gnome-configuration/appearance.nix
+++ modules/home/gnome-configuration/appearance.nix
@@ -5,6 +5,8 @@
       accent-color = "blue";
       gtk-theme = "Adwaita";
       icon-theme = "Adwaita";
+      cursor-theme = "Adwaita";
+      cursor-size = 24;
       font-name = "JetBrainsMono Nerd Font 12";
       document-font-name = "Adwaita Sans 11";
       monospace-font-name = "JetBrainsMono Nerd Font 12";
