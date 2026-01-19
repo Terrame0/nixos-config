e0a3476668159fb35e775d5b512323f2cbf9db10
Terrame0 <terrame_0@proton.me>
2026-01-19 19:31:04 +1000

    [WIP]
    [hostname 'desktop']
    
    [gen 90 | ver 25.11.20260113.2c3e5ec]
    
    [2026-01-19 01:36:53]

--- modules/home/programs/alacritty.nix
+++ modules/home/programs/alacritty.nix
@@ -1,17 +1,73 @@
 {...}: {
+  # for #d7dad8
+  # cm #a7a8a7
+  # red #d77c79
+  # ora #e6a472
+  # yel #f4cf86
+  # gre #c2c77b
+  # aqu #9ac9c4
+  # blu #92b2ca
+  # pur #c0a7c7
+  # 
+  # sel #474c52
+  # cur #35383c
+  # bg #27292c
+  # -- run vscode under
+  environment.sessionVariables.NIXOS_OZONE_WL = "1";
   programs.alacritty = {
     enable = true;
     theme = "vscode";
     settings = {
+      colors = {
+        transparent_background_colors = true;
+        primary ={
+          foreground = "#d7dad8";
+          background = "#27292c";
+
+        };
+        search = {
+          matches = {
+            foreground = "#a7a8a7";
+            background = "#474c52";
+          };
+          focused_match = {
+            foreground = "#d7dad8";
+            background = "#474c52";
+          };
+        };
+        line_indicator = {
+          foreground = "#d7dad8";
+          background = "#474c52"; 
+        };
+        footer_bar = {
+          foreground = "#d7dad8";
+          background = "#474c52"; 
+        };
+        selection = {
+          foreground = "#d7dad8";
+          background = "#474c52"; 
+        };
+        normal = {
+          black = "#27292c";
+          red = "#d77c79";
+          green = "#c2c77b";
+          yellow = "#f4cf86";
+          blue = "#92b2ca";
+          magenta = "#c0a7c7";
+          cyan = "#9ac9c4";
+          white = "#d7dad8";
+        };
+      };
       window = {
         padding = {
           x = 8;
           y = 8;
         };
         dynamic_padding = true;
-        opacity = 0.5;
+        opacity = 0.85;
         startup_mode = "Maximized";
         title = "Terminal";
+        blur = true;
       };
       keyboard.bindings = [
         # -- latin keybinds
--- modules/home/programs/kitty.nix
+++ modules/home/programs/kitty.nix
@@ -6,8 +6,8 @@
     settings = {
       window_padding_width = "8 8";
       resize_debounce_time = "0.1";
-      background_opacity = "0.8";
-      blur = true;
+      # background_opacity = "0.8";
+      # blur = true;
       font_family = "JetBrainsMono Nerd Font";
       font_size = 14.0;
       cursor_shape = "beam";
