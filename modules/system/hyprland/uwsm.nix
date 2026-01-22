3c87786c592590a48f66101da9656d3d01e995af
Terrame0 <terrame_0@proton.me>
2026-01-22 19:08:46 +1000

    [WIP]
    [hostname 'desktop']
    
    [gen 163 | ver 25.11.20260117.72ac591]
    
    [2026-01-22 18:26:55]

--- modules/system/hyprland/uwsm.nix
+++ modules/system/hyprland/uwsm.nix
@@ -1,9 +1,11 @@
-{...}: {
+{lib,...}: {
   programs.uwsm = {
     enable = true;
     waylandCompositors = {
       hyprland = {
-        binPath = "/run/current-system/sw/bin/start-hyprland";
+        prettyName = lib.mkForce "[Hyprland]";
+        comment = lib.mkForce "[Hyprland managed by UWSM]";
+        binPath = lib.mkForce "/run/current-system/sw/bin/start-hyprland";
       };
     };
   };
