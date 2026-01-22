0487e78a73485426bd878d5bfbe9b67e980b3858
Terrame0 <terrame_0@proton.me>
2026-01-22 19:06:55 +1000

    [WIP]
    [hostname 'desktop']
    
    [gen 163 | ver 25.11.20260117.72ac591]
    
    [2026-01-22 18:26:55]

--- modules/system/hyprland/uwsm.nix
+++ modules/system/hyprland/uwsm.nix
@@ -3,8 +3,6 @@
     enable = true;
     waylandCompositors = {
       hyprland = {
-        prettyName = "[Hyprland]";
-        comment = "[Hyprland managed by UWSM]";
         binPath = "/run/current-system/sw/bin/start-hyprland";
       };
     };
