36af67e514e8e022cc31a93ab116b5e42ead792b
Terrame0 <terrame_0@proton.me>
2026-01-22 18:04:06 +1000

    [FUNCTIONAL]
    [hostname 'desktop']
    
    [gen 160 | ver 25.11.20260117.72ac591]
    
    [2026-01-22 18:04:04]

--- modules/home/hyprland-config/symlink-configs.nix
+++ modules/home/hyprland-config/symlink-configs.nix
@@ -1,15 +1,14 @@
-{ lib, ... }:
-let
-  config-directories = lib.filterAttrs 
-  (_: type: type == "directory") 
-  (builtins.readDir ./config);
-in
-{
+{lib, ...}: let
+  config-directories =
+    lib.filterAttrs
+    (_: type: type == "directory")
+    (builtins.readDir ./config);
+in {
   xdg.configFile =
     lib.mapAttrs
-      (name: _: {
-        source = ./config/${name};
-        recursive = true;
-      })
-      config-directories;
+    (name: _: {
+      source = ./config/${name};
+      recursive = true;
+    })
+    config-directories;
 }
