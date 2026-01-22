2fd48f97ed8d273f7159330115918a9d513e436a
Terrame0 <terrame_0@proton.me>
2026-01-22 19:25:26 +1000

    [WIP]
    [hostname 'desktop']
    
    [gen 164 | ver 25.11.20260117.72ac591]
    
    [2026-01-22 19:09:04]

--- modules/system/hyprland/uwsm.nix
+++ modules/system/hyprland/uwsm.nix
@@ -1,10 +1,8 @@
-{lib,...}: {
+{lib, ...}: {
   programs.uwsm = {
     enable = true;
     waylandCompositors = {
       hyprland = {
-        prettyName = lib.mkForce "[Hyprland]";
-        comment = lib.mkForce "[Hyprland managed by UWSM]";
         binPath = lib.mkForce "/run/current-system/sw/bin/start-hyprland";
       };
     };
---
+++ modules/system/services/greetd.nix
@@ -0,0 +1,15 @@
+{ lib, config, ... }:
+{
+  # -- duplicating this option so the package
+  # -- is available in system configuration
+  programs.uwsm.enable = true;
+  services.greetd = {
+    enable = true;
+    settings = {
+      default_session = {
+        command = "${lib.getExe config.programs.uwsm.package} start -e -D Hyprland hyprland-uwsm.desktop";
+        user = "terrame";
+      };
+    };
+  };
+}
