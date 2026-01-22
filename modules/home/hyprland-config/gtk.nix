ee9e2dd9549fca25ecd12a86867ca6b01606f0b0
Terrame0 <terrame_0@proton.me>
2026-01-22 19:39:15 +1000

    [WIP]
    [hostname 'desktop']
    
    [gen 166 | ver 25.11.20260117.72ac591]
    
    [2026-01-22 19:35:43]

--- modules/home/hyprland-config/gtk.nix
+++ modules/home/hyprland-config/gtk.nix
@@ -15,21 +15,21 @@
     };
   };
 
-  #xdg.configFile."gtk-3.0/settings.ini".text = ''
-  #  [Settings]
-  #  gtk-theme-name=Adwaita-dark
-  #  gtk-icon-theme-name=Adwaita
-  #  gtk-cursor-theme-name=Adwaita
-  #  gtk-cursor-theme-size=24
-  #'';
-  #
-  #xdg.configFile."gtk-4.0/settings.ini".text = ''
-  #  [Settings]
-  #  gtk-theme-name=Adwaita-dark
-  #  gtk-icon-theme-name=Adwaita
-  #  gtk-cursor-theme-name=Adwaita
-  #  gtk-cursor-theme-size=24
-  #'';
+  xdg.configFile."gtk-3.0/settings.ini".text = ''
+    [Settings]
+    gtk-theme-name=Adwaita-dark
+    gtk-icon-theme-name=Adwaita
+    gtk-cursor-theme-name=Adwaita
+    gtk-cursor-theme-size=24
+  '';
+  
+  xdg.configFile."gtk-4.0/settings.ini".text = ''
+    [Settings]
+    gtk-theme-name=Adwaita-dark
+    gtk-icon-theme-name=Adwaita
+    gtk-cursor-theme-name=Adwaita
+    gtk-cursor-theme-size=24
+  '';
 
   home.sessionVariables = {
     XCURSOR_THEME = "Adwaita";
--- modules/system/hyprland/hyprland.nix
+++ modules/system/hyprland/hyprland.nix
@@ -3,7 +3,6 @@
   services.xserver = {
     enable = true;
     videoDrivers = ["nvidia"];
-    displayManager.startx.enable = true;
   };
 
   environment.sessionVariables = {
