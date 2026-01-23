f0e69c93de4e6aba73eb1bf6165836f4a4aecbe4
Terrame0 <terrame_0@proton.me>
2026-01-23 20:50:15 +1000

    [FUNCTIONAL]
    [hostname 'desktop']
    
    [gen 243 | ver 25.11.20260117.72ac591]
    
    [2026-01-23 20:50:13]

--- modules/system/hyprland/hyprland.nix
+++ modules/system/hyprland/hyprland.nix
@@ -19,17 +19,17 @@
   # };
 
   environment.sessionVariables = {
-  # -- force VSCode (and XWayland apps) to use X11
-  NIXOS_OZONE_WL = "0";        # Electron uses X11
-  GDK_BACKEND = "x11";         # GTK apps X11
-  QT_QPA_PLATFORM = "xcb";     # Qt apps X11
-  SDL_VIDEODRIVER = "x11";     # SDL apps X11
-  CLUTTER_BACKEND = "x11";     # GTK3D apps X11
+    # -- force VSCode (and XWayland apps) to use X11
+    NIXOS_OZONE_WL = "0"; # Electron uses X11
+    GDK_BACKEND = "x11"; # GTK apps X11
+    QT_QPA_PLATFORM = "xcb"; # Qt apps X11
+    SDL_VIDEODRIVER = "x11"; # SDL apps X11
+    CLUTTER_BACKEND = "x11"; # GTK3D apps X11
 
-  # -- force NVIDIA drivers
-  LIBVA_DRIVER_NAME = "nvidia";
-  __GLX_VENDOR_LIBRARY_NAME = "nvidia";
-};
+    # -- force NVIDIA drivers
+    LIBVA_DRIVER_NAME = "nvidia";
+    __GLX_VENDOR_LIBRARY_NAME = "nvidia";
+  };
 
   # -- using an unstable version (overridden in an overlay)
   programs.hyprland = {
