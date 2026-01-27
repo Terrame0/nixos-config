deb883940701c20851f4653da3af2b1c2eb06303
Terrame0 <terrame_0@proton.me>
2026-01-27 18:51:42 +1000

    [WIP]
    [hostname 'desktop']
    
    [gen 274 | ver 25.11.20260117.72ac591]
    
    [2026-01-27 14:31:11]

--- modules/home/programs/vs-code.nix
+++ modules/home/programs/vs-code.nix
@@ -51,12 +51,60 @@
 
       userSettings = {
         # ============================================================
-        # -- nix linting
+        # -- nix language server integration
         # ============================================================
+
         "nix.enableLanguageServer" = true;
-        "nix.formatterPath" = "alejandra";
         "nix.serverPath" = "nixd";
-        "nix.hiddenLanguageServerErrors" = ["textDocument/definition"];
+        "nix.serverSettings" = let
+          flake = builtins.getFlake (builtins.toString ./.);
+
+          # -- nixpkgs import
+          nixpkgs-expression = import flake.inputs.nixpkgs {system = builtins.currentSystem;};
+
+          # -- nixos options
+          nixos-expression = let
+            configs = flake.nixosConfigurations or {};
+            first-config =
+              if builtins.length (builtins.attrNames configs) > 0
+              then configs.${builtins.head (builtins.attrNames configs)}
+              else {};
+            result = builtins.tryEval (first-config.options or {});
+          in
+            if result.success
+            then result.value
+            else {};
+
+          # -- hm options
+          home-manager-expression = let
+            configs = flake.homeConfigurations or {};
+            first-config =
+              if builtins.length (builtins.attrNames configs) > 0
+              then configs.${builtins.head (builtins.attrNames configs)}
+              else {};
+            result = builtins.tryEval (first-config.options or {});
+          in
+            if result.success
+            then result.value
+            else {};
+        in {
+          "nixd" = {
+            "nixpkgs" = {
+              "expr" = nixpkgs-expression;
+            };
+            "options" = {
+              "nixos" = {
+                "expr" = nixos-expression;
+              };
+              "home-manager" = {
+                "expr" = home-manager-expression;
+              };
+            };
+            "formatting" = {
+              "command" = ["alejandra"];
+            };
+          };
+        };
 
         # ============================================================
         # -- misc behavior
