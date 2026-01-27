a925829bd1ea0e91d15bd0f511d8aece2002ce85
Terrame0 <terrame_0@proton.me>
2026-01-27 21:35:59 +1000

    [WIP]
    [hostname 'desktop']
    
    [gen 283 | ver 25.11.20260117.72ac591]
    
    [2026-01-27 21:33:47]

--- modules/home/programs/vs-code.nix
+++ modules/home/programs/vs-code.nix
@@ -62,14 +62,14 @@
               "command" = ["alejandra"];
             };
             "nixpkgs" = {
-              "expr" = "import '\${flake.inputs.nixpkgs}' { }";
+              "expr" = "import \"\${flake.inputs.nixpkgs}\" { }";
             };
             "options" = {
               "nixos" = {
-                "expr" = "(let pkgs = import '\${inputs.nixpkgs}' { }; in (pkgs.lib.evalModules { modules =  (import '\${inputs.nixpkgs}/nixos/modules/module-list.nix') ++ [ ({...}: { nixpkgs.hostPlatform = builtins.currentSystem;} ) ] ; })).options";
+                "expr" = "(let pkgs = import \"\${inputs.nixpkgs}\" { }; in (pkgs.lib.evalModules { modules =  (import \"\${inputs.nixpkgs}/nixos/modules/module-list.nix\") ++ [ ({...}: { nixpkgs.hostPlatform = builtins.currentSystem;} ) ] ; })).options";
               };
               "home_manager" = {
-                "expr" = "(let pkgs = import '\${inputs.nixpkgs}' { }; lib = import '\${inputs.home-manager}/modules/lib/stdlib-extended.nix' pkgs.lib; in (lib.evalModules { modules =  (import '\{inputs.home-manager}/modules/modules.nix') { inherit lib pkgs; check = false; }; })).options";
+                "expr" = "(let pkgs = import \"\${inputs.nixpkgs}\" { }; lib = import \"\${inputs.home-manager}/modules/lib/stdlib-extended.nix\" pkgs.lib; in (lib.evalModules { modules =  (import \"\{inputs.home-manager}/modules/modules.nix\") { inherit lib pkgs; check = false; }; })).options";
               };
             };
           };
