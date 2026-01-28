dda7ceecb929e8317d5297620b0f2383e84ec605
Terrame0 <terrame_0@proton.me>
2026-01-29 05:21:40 +1000

    [FUNCTIONAL]
    [hostname 'laptop']
    
    [gen 149 | ver 25.11.20260125.1cd347b]
    
    [2026-01-29 05:21:38]

--- modules/home/desktop-environment/gtk.nix
+++ modules/home/desktop-environment/gtk.nix
@@ -5,11 +5,10 @@
   ];
 
   dconf.settings = {
-  "org/gnome/desktop/interface" = {
-    color-scheme = "prefer-dark";
+    "org/gnome/desktop/interface" = {
+      color-scheme = "prefer-dark";
+    };
   };
-};
-
 
   gtk = {
     enable = true;
