1cfd9daba5650f5c6d9841a9b1c3bf46f282dca6
Terrame0 <terrame_0@proton.me>
2026-01-29 05:18:01 +1000

    [WIP]
    [hostname 'laptop']
    
    [gen 146 | ver 25.11.20260125.1cd347b]
    
    [2026-01-29 04:16:31]

--- modules/home/desktop-environment/gtk.nix
+++ modules/home/desktop-environment/gtk.nix
@@ -3,33 +3,26 @@
     adwaita-icon-theme
     gnome-themes-extra
   ];
-
   gtk = {
     enable = true;
-    theme.name = "Adwaita-dark";
-    iconTheme.name = "Adwaita";
+    theme = {
+      name = "Adwaita";
+    };
+    iconTheme = {
+      name = "Adwaita";
+    };
     cursorTheme = {
       name = "Adwaita";
       package = pkgs.adwaita-icon-theme;
       size = 24;
     };
+    gtk3.extraConfig = {
+      gtk-application-prefer-dark-theme = true;
+    };
+    gtk4.extraConfig = {
+      gtk-application-prefer-dark-theme = true;
+    };
   };
-
-  xdg.configFile."gtk-3.0/settings.ini".text = ''
-    [Settings]
-    gtk-theme-name=Adwaita-dark
-    gtk-icon-theme-name=Adwaita
-    gtk-cursor-theme-name=Adwaita
-    gtk-cursor-theme-size=24
-  '';
-
-  xdg.configFile."gtk-4.0/settings.ini".text = ''
-    [Settings]
-    gtk-theme-name=Adwaita-dark
-    gtk-icon-theme-name=Adwaita
-    gtk-cursor-theme-name=Adwaita
-    gtk-cursor-theme-size=24
-  '';
 
   home.sessionVariables = {
     XCURSOR_THEME = "Adwaita";
