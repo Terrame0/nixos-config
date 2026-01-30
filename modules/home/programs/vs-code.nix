021d9eaf0bd74bd99e4e82c331c97fe85751ca4e
Terrame0 <terrame_0@proton.me>
2026-01-31 02:39:29 +1000

    [WIP]
    [hostname 'desktop']
    
    [gen 314 | ver 25.11.20260125.1cd347b]
    
    [2026-01-30 15:01:52]

--- modules/home/programs/vs-code.nix
+++ modules/home/programs/vs-code.nix
@@ -1,7 +1,7 @@
 {pkgs, ...}: {
   programs.vscode = {
     enable = true;
-    package = pkgs.vscode-fhs;
+    package = pkgs.vscode;
     mutableExtensionsDir = false;
 
     profiles.default = let
@@ -48,6 +48,14 @@
       # ============================================================
 
       userSettings = {
+
+        # ============================================================
+        # -- fixing inconsistencies in the integrated terminal
+        # ============================================================
+
+        # "terminal.integrated.inheritEnv" = false;
+        # "C_Cpp.default.includePath" = [];
+
         # ============================================================
         # -- clangd settings
         # ============================================================
