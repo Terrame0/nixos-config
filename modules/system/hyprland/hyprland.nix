c0e035835b03467901660ba3f5f7c1817dc9a6f8
Terrame0 <terrame_0@proton.me>
2026-02-21 20:14:08 +1000

    [WIP]
    [hostname 'desktop']
    
    [gen 388 | ver 25.11.20260209.2db38e0]
    
    [2026-02-21 19:39:11]

--- modules/system/hyprland/hyprland.nix
+++ modules/system/hyprland/hyprland.nix
@@ -13,12 +13,6 @@
     QT_QPA_PLATFORM = "wayland;xcb";
     SDL_VIDEODRIVER = "wayland";
     CLUTTER_BACKEND = "wayland";
-    # -- force the use of nvidia drivers
-    LIBVA_DRIVER_NAME = "nvidia";
-    __GLX_VENDOR_LIBRARY_NAME = "nvidia";
-    # -- egl shits the bed without these
-    __EGL_VENDOR_LIBRARY_FILENAMES = "/run/opengl-driver/share/glvnd/egl_vendor.d/10_nvidia.json";
-    __EGL_EXTERNAL_PLATFORM_CONFIG_DIRS = "/run/opengl-driver/share/egl/egl_external_platform.d";
   };
 
   # -- using an unstable version (overridden in an overlay)
