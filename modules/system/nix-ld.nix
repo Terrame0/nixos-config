1300b0d5a84a8c7062e9429fe71e2342e5b8564a
Terrame0 <terrame_0@proton.me>
2026-02-21 19:12:19 +1000

    [WIP]
    [hostname 'desktop']
    
    [gen 385 | ver 25.11.20260209.2db38e0]
    
    [2026-02-21 18:50:09]

--- modules/system/nix-ld.nix
+++ modules/system/nix-ld.nix
@@ -81,14 +81,20 @@
       speex
 
       # -- SDL
-      SDL
+      sdl3
+      sdl2-compat
+      sdl3-image
+      sdl3-ttf
+      
       SDL2
-      SDL_image
-      SDL_ttf
-      SDL_mixer
       SDL2_image
       SDL2_ttf
       SDL2_mixer
+
+      SDL
+      SDL_image
+      SDL_ttf
+      SDL_mixer
 
       # -- system / runtime
       coreutils
