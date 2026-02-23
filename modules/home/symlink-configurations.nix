a32eaa2083794c44c49d4b8f079b6b70855f848e
Terrame0 <terrame_0@proton.me>
2026-02-23 21:27:13 +1000

    [FUNCTIONAL]
    [hostname 'desktop']
    
    [gen 419 | ver 25.11.20260209.2db38e0]
    
    [2026-02-23 21:27:11]

--- modules/home/symlink-configurations.nix
+++ modules/home/symlink-configurations.nix
@@ -1,19 +1,23 @@
-{lib,pkgs,...}: let
+{
+  lib,
+  pkgs,
+  ...
+}: let
   config-path = ./configurations;
 
   collect-files = base-path: rel: let
     dir = builtins.readDir (base-path + "/${rel}");
-    
-  # -- compiles scss files to css
-  scss-compiler = {src-file}: let
-    name = builtins.basename src-file;
-  in
-    pkgs.runCommand
-    {buildInputs = with pkgs; [sassc];}
-    ''
-      basename="${name}"
-      sassc ${src-file} "${src-file}.css"
-    '';
+
+    # -- compiles scss files to css
+    scss-compiler = {src-file}: let
+      name = builtins.basename src-file;
+    in
+      pkgs.runCommand
+      {buildInputs = with pkgs; [sassc];}
+      ''
+        basename="${name}"
+        sassc ${src-file} "${src-file}.css"
+      '';
   in
     lib.concatLists (lib.mapAttrsToList (
         name: type: let
