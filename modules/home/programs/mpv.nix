f7d1029b9e39cc261926e6c7ecd1f2d826096509
Terrame0 <terrame_0@proton.me>
2026-02-26 00:17:36 +1000

    [WIP]
    [hostname 'desktop']
    
    [gen 441 | ver 25.11.20260209.2db38e0]
    
    [2026-02-25 01:02:40]

--- modules/home/programs/mpv.nix
+++ modules/home/programs/mpv.nix
@@ -1,3 +1,16 @@
-{...}: {
-  programs.mpv.enable = true;
+{pkgs,...}: {
+  programs.mpv = {
+    enable = true;
+    scripts = with pkgs.mpvScripts; [ 
+      uosc
+      thumbfast 
+      youtube-upnext
+      webtorrent-mpv-hook
+      command_palette
+      playlistmanager
+      autosave-state
+      sponsorblock
+      file-browser
+    ];
+  };
 }
