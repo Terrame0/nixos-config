b0a0c86bb5dd6988bf08e655a232e8a673273af4
Terrame0 <terrame_0@proton.me>
2026-02-26 01:55:33 +1000

    [WIP]
    [hostname 'desktop']
    
    [gen 447 | ver 25.11.20260209.2db38e0]
    
    [2026-02-26 01:41:48]

--- modules/home/programs/mpv.nix
+++ modules/home/programs/mpv.nix
@@ -13,5 +13,19 @@
 
       evafast
     ];
+
+    config = {
+      profile = "gpu-hq";
+      force-window = true;
+      ytdl-format = "bestvideo+bestaudio";
+      cache-default = 4000000;
+      gpu-api="vulkan";
+      hwdec="auto";
+      cache="yes";
+      cache-secs="30";
+      demuxer-max-bytes="500MiB";
+      demuxer-readahead-secs="20";
+      idle="yes";
+    };
   };
 }
