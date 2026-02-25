b96dbf913a380cc4321a04aff4ae40f868269b8c
Terrame0 <terrame_0@proton.me>
2026-02-26 01:46:50 +1000

    [WIP]
    [hostname 'desktop']
    
    [gen 447 | ver 25.11.20260209.2db38e0]
    
    [2026-02-26 01:41:48]

--- modules/home/programs/mpv.nix
+++ modules/home/programs/mpv.nix
@@ -3,18 +3,15 @@
     enable = true;
     scripts = with pkgs.mpvScripts; [
       uosc
-
+      ff2mpv
       thumbfast
       youtube-upnext
       webtorrent-mpv-hook
 
       mpv-playlistmanager
       mpris
-      sponsorblock
 
       evafast
-      eisa01.undoredo
-      eisa01.smartskip
     ];
   };
 }
