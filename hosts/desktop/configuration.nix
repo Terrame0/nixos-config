6710542650aa40370584216a4315d50af2339245
Terrame0 <terrame_0@proton.me>
2026-02-26 21:03:44 +1000

    [WIP]
    [hostname 'desktop']
    
    [gen 460 | ver 25.11.20260209.2db38e0]
    
    [2026-02-26 19:07:27]

--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -1,7 +1,8 @@
 {lib, ...}: {
   # -- importing modules and other stuff
   imports = lib.filter (path: lib.hasSuffix ".nix" (toString path)) (
-    lib.filesystem.listFilesRecursive ../../modules/system
+    (lib.filesystem.listFilesRecursive ../../modules/system) ++
+    (lib.filesystem.listFilesRecursive ../../modules/lib)
   );
 
   # -- system state version
