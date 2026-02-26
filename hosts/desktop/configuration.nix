ed38f12579b9df712b30d225f6f4f1643b21258b
Terrame0 <terrame_0@proton.me>
2026-02-26 21:20:51 +1000

    [WIP]
    [hostname 'desktop']
    
    [gen 460 | ver 25.11.20260209.2db38e0]
    
    [2026-02-26 19:07:27]

--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -1,14 +1,4 @@
 {lib, ...}: {
-  _module.args = {
-    config-add = namespace: args: {
-      options.${namespace} = lib.mkOption {
-        type = lib.types.anything;
-        description = namespace;
-      };
-      config.${namespace} = args;
-    };
-  };
-
   imports = lib.filter (path: lib.hasSuffix ".nix" (toString path)) (
     (lib.filesystem.listFilesRecursive ../../modules/system)
     ++ (lib.filesystem.listFilesRecursive ../../modules/lib)
