7525533633d2ae46b314138e3868642024227f30
Terrame0 <terrame_0@proton.me>
2026-03-02 16:37:19 +1000

    [WIP]
    [hostname 'desktop']
    
    [gen 496 | ver 25.11.20260209.2db38e0]
    
    [2026-03-01 18:17:52]

--- home-dir/.config/waybar/clock.sh
+++ home-dir/.config/waybar/clock.sh
@@ -4,7 +4,7 @@ mon=$(date +"%b")
 mon="${mon^}"
 
 colon="#[str|config.make-span {color = config.palette.comment;} ":"]"
-sep="#[str|config.make-span {color = config.palette.blue;} "==="]"
+sep="#[str|config.make-span {color = config.palette.purple;} "==="]"
 point="#[str|config.make-span {color = config.palette.comment;} "·"]"
 
 if (( 10#$sec % 2 == 0 )); then
