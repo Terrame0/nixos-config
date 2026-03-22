f8833d4eb2f29601008a0d4a2795cefe513f79e4
Terrame0 <terrame_0@proton.me>
2026-03-22 14:43:22 +1000

    [WIP]
    [hostname 'desktop']
    
    [gen 516 | ver 25.11.20260302.c581273]
    
    [2026-03-20 20:41:27]

--- modules/system/overlays/unstable-packages.nix
+++ modules/system/overlays/unstable-packages.nix
@@ -1,17 +1,17 @@
 {pkgs, ...}: let
-  unstable =
-    import
-    (fetchTarball {
-      url = "https://github.com/NixOS/nixpkgs/archive/nixos-unstable.tar.gz";
-      sha256 = "03plivnr4cg0h8v7djf9g2jra09r45pmdiirmy4lvl2n1d4yb7ac";
-    })
-    {
-      system = pkgs.stdenv.hostPlatform.system;
-      config = {};
-    };
-  overlay = final: prev: {
-    hyprland = unstable.hyprland;
-  };
+  # unstable =
+  #   import
+  #   (fetchTarball {
+  #     url = "https://github.com/NixOS/nixpkgs/archive/nixos-unstable.tar.gz";
+  #     sha256 = "03plivnr4cg0h8v7djf9g2jra09r45pmdiirmy4lvl2n1d4yb7ac";
+  #   })
+  #   {
+  #     system = pkgs.stdenv.hostPlatform.system;
+  #     config = {};
+  #   };
+  # overlay = final: prev: {
+  #   hyprland = unstable.hyprland;
+  # };
 in {
-  nixpkgs.overlays = [overlay];
+  # nixpkgs.overlays = [overlay];
 }
