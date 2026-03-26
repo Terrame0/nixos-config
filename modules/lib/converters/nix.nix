8fbe3e73a38fde8792041349665d493b8f0ecc1c
Terrame0 <terrame_0@proton.me>
2026-03-26 21:19:30 +1000

    [WIP]
    [hostname 'desktop']
    
    [gen 529 | ver 25.11.20260302.c581273]
    
    [2026-03-26 01:25:20]

--- modules/lib/converters/nix.nix
+++ modules/lib/converters/nix.nix
@@ -7,7 +7,7 @@
 extend-config "convert"
 {
   nix = file: let
-    generator = lib.head (config.file.get-specs "to" file);
+    generator = lib.head (config.file.get-spec-values "to" file);
     generator-function = lib.generators.${"to" + (lib.toUpper generator)} {};
     contents = config.string.evaluate-nix (config.file.read file);
   in (config.file.emplace file (generator-function contents));
