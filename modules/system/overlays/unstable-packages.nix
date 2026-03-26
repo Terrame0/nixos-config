a45ffe3922095211cf4d8c92addd84ea27c2e9c8
Terrame0 <terrame_0@proton.me>
2026-03-26 12:33:50 +1000

    [WIP]
    [hostname 'laptop']
    
    [gen 238 | ver 25.11.20260302.c581273]
    
    [2026-03-21 17:17:53]

--- modules/system/overlays/unstable-packages.nix
+++ modules/system/overlays/unstable-packages.nix
@@ -7,7 +7,7 @@
     import
     (fetchTarball {
       url = "https://github.com/NixOS/nixpkgs/archive/nixos-unstable.tar.gz";
-      sha256 = "03plivnr4cg0h8v7djf9g2jra09r45pmdiirmy4lvl2n1d4yb7ac";
+      sha256 = "1dx1giygw794fhmzaw14xda3ghv5393fx1icx911ffxfmbdia472";
     })
     {
       system = pkgs.stdenv.hostPlatform.system;
