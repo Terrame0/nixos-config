ac79423a3cd0598ddd4a9e0739408f6168d902b5
Terrame0 <terrame_0@proton.me>
2026-05-01 14:46:32 +1000

    [WIP]
    [hostname 'laptop']
    
    [gen 297 | ver 25.11.20260429.755f5aa]
    
    [2026-05-01 14:45:17]

--- modules/home/desktop-environment/hyprpaper.nix
+++ modules/home/desktop-environment/hyprpaper.nix
@@ -1,25 +1,21 @@
-{pkgs, ...}: {
+{pkgs, ...}: let
+  wallpaper = pkgs.fetchurl {
+    url = "https://w.wallhaven.cc/full/yj/wallhaven-yj9357.jpg";
+    sha256 = "sha256-OAcXF/YIDD3ZTF0OBaNiQ+Xsc1Fa35U8G65l45745IE=";
+  };
+in {
   services.hyprpaper = {
     enable = true;
-    settings = let
-      wallpaper = pkgs.fetchurl {
-        url = "https://w.wallhaven.cc/full/yj/wallhaven-yj9357.jpg";
-        sha256 = "sha256-OAcXF/YIDD3ZTF0OBaNiQ+Xsc1Fa35U8G65l45745IE=";
-        #url = "https://w.wallhaven.cc/full/p9/wallhaven-p9m2qm.jpg";
-        #sha256 = "sha256-NLEKwYZ4gQFmYDU12sPgReP5EdtPuyor27gIeaVS60k=";
-      };
-    in {
-      ipc = "on";
-      splash = false;
-      preload = [
-        "${wallpaper}"
-      ];
-      wallpaper = [
-        {
-          monitor = "";
-          path = "${wallpaper}";
-        }
-      ];
-    };
+
+    ipc = "on";
+    splash = false;
+
+    preload = [
+      "${wallpaper}"
+    ];
+
+    wallpaper = [
+      ",${wallpaper}"
+    ];
   };
 }
