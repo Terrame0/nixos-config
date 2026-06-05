92c53cfd9fce7bc70ed2266cc4d0f46ef6459417
Terrame0 <terrame_0@proton.me>
2026-06-05 17:19:23 +1000

    [WIP]
    [hostname 'laptop']
    
    [gen 307 | ver 25.11.20260510.8fd9daa]
    
    [2026-06-05 16:56:54]

--- system/modules/core/hardware/networking.nix
+++ system/modules/core/hardware/networking.nix
@@ -5,9 +5,9 @@
     networkmanager.enable = true;
     firewall.allowedTCPPorts = [5432];
     # -- nix breaks if system proxy is enabled
-    # proxy = {
-    #   default = "http://127.0.0.1:10808";
-    #   noProxy = "127.0.0.1,localhost,192.168.0.0/16,10.0.0.0/8"; # -- failsafe
-    # };
+    proxy = {
+      default = "http://127.0.0.1:10808";
+      noProxy = "127.0.0.1,localhost,192.168.0.0/16,10.0.0.0/8"; # -- failsafe
+    };
   };
 }
