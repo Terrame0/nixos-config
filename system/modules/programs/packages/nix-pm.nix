8b1374160533360649ed0785e979ea727b10f324
Terrame0 <terrame_0@proton.me>
2026-06-06 15:16:57 +1000

    [WIP]
    [hostname 'laptop']
    
    [gen 313 | ver 26.05.20260603.6b31628]
    
    [2026-06-06 14:57:38]

--- system/modules/programs/packages/nix-pm.nix
+++ system/modules/programs/packages/nix-pm.nix
@@ -1,7 +1,6 @@
 {...}: {
   nix = {
     settings = {
-      auto-optimise-store = true;
       experimental-features = [
         "nix-command"
         "flakes"
@@ -11,11 +10,6 @@
         "https://mirror.sjtu.edu.cn/nix-channels/store"
         "https://mirrors.ustc.edu.cn/nix-channels/store"
       ];
-    };
-    gc = {
-      automatic = true;
-      dates = "weekly";
-      options = "--delete-older-than 7d";
     };
   };
 }
