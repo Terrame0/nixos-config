6412cb648ad9fb185059ecdde181ef809e2dd73d
Terrame0 <terrame_0@proton.me>
2026-06-06 17:03:17 +1000

    [WIP]
    [hostname 'laptop']
    
    [gen 316 | ver 26.05.20260603.6b31628]
    
    [2026-06-06 17:02:22]

--- system/modules/programs/packages/nix-pm.nix
+++ system/modules/programs/packages/nix-pm.nix
@@ -6,9 +6,15 @@
         "flakes"
       ];
       substituters = [
+        "https://cache.nixos.org"
+        "https://hyprland.cachix.org"
         "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store"
         "https://mirror.sjtu.edu.cn/nix-channels/store"
         "https://mirrors.ustc.edu.cn/nix-channels/store"
+      ];
+      trusted-public-keys = [
+        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+        "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
       ];
     };
   };
