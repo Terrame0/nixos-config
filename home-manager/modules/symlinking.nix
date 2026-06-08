48bb2e8f9488f9392a57a562be5c9b7cdd6a463c
Terrame0 <terrame_0@proton.me>
2026-06-08 16:56:13 +1000

    [WIP]
    [hostname 'laptop']
    
    [gen 320 | ver 26.05.20260603.6b31628]
    
    [2026-06-07 10:58:41]

--- home-manager/modules/symlinking.nix
+++ home-manager/modules/symlinking.nix
@@ -4,11 +4,33 @@
   flake-root,
   ...
 }: {
-  home.file = lib.pipe "${flake-root}/home-manager/symlinking/home" [
-    mlem.vfs.dir.from-real
-    (mlem.vfs.dir.collapse (
-      path: file: {"test-dir/${mlem.vfs.path.get.str path}".text = file.contents;}
-    ))
-    mlem.attrs.merge.recursive.no-collision
-  ];
+  home.file =
+    lib.pipe
+    (mlem.attrs.resolve-deps {
+      imported = {
+        transform = _:
+          mlem.vfs.dir.from-src "${flake-root}/home-manager/symlinking/home";
+      };
+      specs-resolved-stripped = {
+        deps = ["imported"];
+        transform = prev: mlem.vfs.dir.resolve-specs {strip = true;} prev.imported;
+      };
+      specs-resolved = {
+        deps = ["imported"];
+        transform = prev: mlem.vfs.dir.resolve-specs {strip = false;} prev.imported;
+      };
+      sass-includes = {
+        deps = ["specs-resolved-stripped"];
+        transform = prev: let
+          dir = prev.specs-resolved-stripped;
+        in
+          mlem.vfs.dir.graft-by-spec {i = "sass";} dir;
+      };
+    })
+    [
+      (mlem.vfs.dir.collapse (
+        path: file: {"test-dir/${mlem.vfs.path.get.str path}".text = file.contents;}
+      ))
+      mlem.attrs.merge.recursive.no-collision
+    ];
 }
