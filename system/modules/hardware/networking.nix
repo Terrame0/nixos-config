2f78f47aba83969deb287a44d46c7372d63be9e1
Terrame0 <terrame_0@proton.me>
2026-06-12 21:50:22 +1000

    [WIP]
    [hostname 'laptop']
    
    [gen 338 | ver 26.05.20260603.6b31628]
    
    [2026-06-12 21:34:29]

--- system/modules/hardware/networking.nix
+++ system/modules/hardware/networking.nix
@@ -3,7 +3,10 @@
     hostName = host.name;
     nameservers = ["8.8.8.8" "8.8.4.4" "1.1.1.1"];
     networkmanager.enable = true;
-    firewall.allowedTCPPorts = [5432];
+    firewall = {
+      allowedTCPPorts = [5432];
+      trustedInterfaces = ["tun0"];
+    };
     #proxy = {
     #  default = "http://127.0.0.1:8080";
     #  noProxy = "127.0.0.1,localhost,192.168.0.0/16,10.0.0.0/8"; # -- failsafe
