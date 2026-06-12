93004f6849112ed0d797019e9217c9e1ba9c9856
Terrame0 <terrame_0@proton.me>
2026-06-12 22:03:00 +1000

    [WIP]
    [hostname 'laptop']
    
    [gen 339 | ver 26.05.20260603.6b31628]
    
    [2026-06-12 21:51:00]

--- system/modules/hardware/networking.nix
+++ system/modules/hardware/networking.nix
@@ -7,9 +7,5 @@
       allowedTCPPorts = [5432];
       trustedInterfaces = ["tun0"];
     };
-    #proxy = {
-    #  default = "http://127.0.0.1:8080";
-    #  noProxy = "127.0.0.1,localhost,192.168.0.0/16,10.0.0.0/8"; # -- failsafe
-    #};
   };
 }
