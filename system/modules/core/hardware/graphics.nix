f2adc3e678ce9107901910d50b5085dda63d4561
Terrame0 <terrame_0@proton.me>
2026-06-12 18:00:58 +1000

    [WIP]
    [hostname 'laptop']
    
    [gen 329 | ver 26.05.20260603.6b31628]
    
    [2026-06-12 17:52:43]

--- system/modules/core/hardware/graphics.nix
+++ system/modules/core/hardware/graphics.nix
@@ -1,16 +1,15 @@
 {...}: {
-  # -- opengl configuration
-  hardware.graphics = {
-    enable = true;
-    enable32Bit = true;
-  };
-
-  services.xserver.videoDrivers = ["nvidia"];
-  hardware.nvidia = {
-    modesetting.enable = true;
-    powerManagement.enable = true;
-    powerManagement.finegrained = false;
-    nvidiaSettings = true;
-    open = false;
+  hardware = {
+    graphics = {
+      enable = true;
+      enable32Bit = true;
+    };
+    nvidia = {
+      modesetting.enable = true;
+      powerManagement.enable = true;
+      powerManagement.finegrained = false;
+      nvidiaSettings = true;
+      open = false;
+    };
   };
 }
