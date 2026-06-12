a05aca239559e2625215e32fca6b95bd6994d752
Terrame0 <terrame_0@proton.me>
2026-06-12 18:06:53 +1000

    [WIP]
    [hostname 'laptop']
    
    [gen 330 | ver 26.05.20260603.6b31628]
    
    [2026-06-12 18:02:41]

--- home-manager/modules/desktop-environment/autostart.nix
+++ home-manager/modules/desktop-environment/autostart.nix
@@ -1,7 +1,7 @@
 {...}: {
-  programs.zsh.profileExtra = ''
-    if [ -z "$WAYLAND_DISPLAY" ] && [ "$XDG_VTNR" = 1 ]; then
-        exec uwsm start -e -D Hyprland hyprland-uwsm.desktop
+  programs.zsh.loginExtra = ''
+    if uwsm check may-start; then
+      exec uwsm start hyprland-uwsm.desktop
     fi
   '';
 }
