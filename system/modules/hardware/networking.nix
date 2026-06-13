4ecbf9b6c8e98cbd0c27c0969bcaaf5922ee1fc7
Terrame0 <terrame_0@proton.me>
2026-06-13 13:21:34 +1000

    [WIP]
    [hostname 'laptop']
    
    [gen 355 | ver 26.05.20260603.6b31628]
    
    [2026-06-13 13:03:26]

--- system/modules/hardware/networking.nix
+++ system/modules/hardware/networking.nix
@@ -2,7 +2,10 @@
   networking = {
     hostName = host.name;
     nameservers = ["1.1.1.1" "8.8.8.8" "8.8.4.4"];
-    networkmanager.enable = true;
+    networkmanager = {
+      enable = true;
+      dns = "none";
+    };
     firewall = {
       allowedTCPPorts = [5432];
       trustedInterfaces = ["tun0"];
