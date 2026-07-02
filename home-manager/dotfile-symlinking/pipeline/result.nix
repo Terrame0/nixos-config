5ed1dbd4352dd88445263e87ba72c07d409c3b71
Terrame0 <terrame_0@proton.me>
2026-07-02 12:08:32 +1000

    [WIP]
    [hostname 'laptop']
    
    [gen 419 | ver 26.05.20260603.6b31628]
    
    [2026-07-02 01:40:28]

--- home-manager/dotfile-symlinking/pipeline/result.nix
+++ home-manager/dotfile-symlinking/pipeline/result.nix
@@ -11,16 +11,14 @@
     ];
     transform = prev:
       lib.pipe prev [
-        (lib.mapAttrsToList
-          (name: value:
-            sundry.attrs.merge.recursive.no-collision
-            (sundry.vfs.dir.collapse (path: file: {
-                "test-dir/${name}/${sundry.vfs.path.get.str path}" =
-                  if file ? text
-                  then {text = file.text;}
-                  else {source = file.origin;};
-              })
-              value)))
+        lib.attrValues
+        sundry.attrs.merge.recursive.no-collision
+        (sundry.vfs.dir.collapse (path: file: {
+          "test-dir/${sundry.vfs.path.get.str path}" =
+            if file ? text
+            then {text = file.text;}
+            else {source = file.origin;};
+        }))
         sundry.attrs.merge.recursive.no-collision
       ];
   };
