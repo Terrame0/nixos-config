8164add9f121906f0aa36a2e3c52649777997769
Terrame0 <terrame_0@proton.me>
2026-07-04 19:18:33 +1000

    [WIP]
    [hostname 'laptop']
    
    [gen 428 | ver 26.05.20260603.6b31628]
    
    [2026-07-04 19:16:42]

--- system/modules/sing-box/config-parts{x}/sing-box-config/default.nix
+++ system/modules/sing-box/config-parts{x}/sing-box-config/default.nix
@@ -1,10 +1,8 @@
-args @ {pkgs, ...}: let
-  part = name: import (./. + "/${name}.nix") args;
-in
-  (pkgs.formats.json {}).generate "sing-box-config.json" (
-    (part "misc")
-    // (part "dns")
-    // (part "inbounds")
-    // (part "outbounds")
-    // (part "route")
-  )
+args @ {pkgs, ...}:
+(pkgs.formats.json {}).generate "sing-box-config.json" (
+  (import ./misc.nix args)
+  (import ./dns.nix args)
+  (import ./inbounds.nix args)
+  (import ./outbounds.nix args)
+  (import ./route.nix args)
+)
