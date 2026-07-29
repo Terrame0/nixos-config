cfc2b2c61b30b75b9891ea88342d5998b364f842
Terrame0 <terrame_0@proton.me>
2026-07-30 03:15:48 +1000

    [WIP]
    [hostname 'laptop']
    
    [gen 2 | ver 26.05.20260603.6b31628]
    
    [2026-07-29 21:48:52]

--- src/nixos/system{modules:system}/nix.nix
+++ src/nixos/system{modules:system}/nix.nix
@@ -7,6 +7,10 @@
       ];
       substituters = [
         "https://cache.nixos.org"
+        "https://hyprland.cachix.org"
+      ];
+      trusted-public-keys = [
+        "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
       ];
     };
   };
