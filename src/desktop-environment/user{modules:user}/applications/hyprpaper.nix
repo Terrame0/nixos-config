6b343d410763853f3e513a28b52998fb65edd257
Terrame0 <terrame_0@proton.me>
2026-08-23 21:02:23 +1000

    [WIP]
    [hostname 'laptop']
    
    [gen 14 | ver 26.05.20260727.2f5a153]
    
    [2026-08-23 20:50:01]

--- src/desktop-environment/user{modules:user}/applications/hyprpaper.nix
+++ src/desktop-environment/user{modules:user}/applications/hyprpaper.nix
@@ -1,9 +1,25 @@
-{pkgs, ...}: let
-  wallpaper = pkgs.fetchurl {
-    url = "https://upload.wikimedia.org/wikipedia/commons/thumb/5/50/Black_colour.jpg/960px-Black_colour.jpg?utm_source=commons.wikimedia.org&utm_campaign=index&utm_content=thumbnail";
-    sha256 = "sha256-xycExewMjEwV0fElqNHi518JizKsWCj8vD3d1TK1Cv8=";
-    # url = "https://w.wallhaven.cc/full/yj/wallhaven-yj9357.jpg";
-    # sha256 = "sha256-OAcXF/YIDD3ZTF0OBaNiQ+Xsc1Fa35U8G65l45745IE=";
+{
+  pkgs,
+  lib,
+  ...
+}: let
+  wallpapers = {
+    black = pkgs.fetchurl {
+      url = "https://upload.wikimedia.org/wikipedia/commons/thumb/5/50/Black_colour.jpg/960px-Black_colour.jpg?utm_source=commons.wikimedia.org&utm_campaign=index&utm_content=thumbnail";
+      sha256 = "sha256-xycExewMjEwV0fElqNHi518JizKsWCj8vD3d1TK1Cv8=";
+    };
+    valley = pkgs.fetchurl {
+      url = "https://w.wallhaven.cc/full/yj/wallhaven-yj9357.jpg";
+      sha256 = "sha256-OAcXF/YIDD3ZTF0OBaNiQ+Xsc1Fa35U8G65l45745IE=";
+    };
+    nix-black = pkgs.fetchurl {
+      url = "https://w.wallhaven.cc/full/pk/wallhaven-pkrqze.png";
+      sha256 = "sha256-nhIUtCy/Hb8UbuxXeL3l3FMausjQrnjTVi1B3GkL9B8=";
+    };
+    nix-colorful = pkgs.fetchurl {
+      url = "https://w.wallhaven.cc/full/p9/wallhaven-p9pd23.png";
+      sha256 = "sha256-7CMuETntiVUCKhUIdJzX+sf3F47GvuX2a61o4xbEzww=";
+    };
   };
 in {
   services.hyprpaper = {
@@ -11,11 +27,11 @@ in {
     settings = {
       ipc = true;
       splash = false;
-      preload = ["${wallpaper}"];
+      preload = ["${wallpapers.nix-black}"];
       wallpaper = [
         {
           monitor = "";
-          path = "${wallpaper}";
+          path = "${wallpapers.nix-black}";
         }
       ];
     };
