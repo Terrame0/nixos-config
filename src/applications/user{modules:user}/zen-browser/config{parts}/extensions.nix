86d5706ade2e6db0c3efffbeb44952570b0841d7
Terrame0 <terrame_0@proton.me>
2026-08-26 11:35:27 +1000

    [WIP]
    [hostname 'tuf-f17']
    
    [gen 31 | ver 26.05.20260825.f4f6986]
    
    [2026-08-26 10:15:42]

--- src/applications/user{modules:user}/zen-browser/config{parts}/extensions.nix
+++ src/applications/user{modules:user}/zen-browser/config{parts}/extensions.nix
@@ -1,13 +1,15 @@
 # -- implemented as a policy
-{...}: {
-  "uBlock0@raymondhill.net" = {
-    install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
+{...}: let
+  mk-extension = slug: {
+    install_url = "https://addons.mozilla.org/firefox/downloads/latest/${slug}/latest.xpi";
     installation_mode = "force_installed";
   };
-  "ff2mpv@yossarian.net" = {
-    install_url = "https://addons.mozilla.org/firefox/downloads/latest/ff2mpv/latest.xpi";
-    installation_mode = "force_installed";
-  };
+in {
+  "uBlock0@raymondhill.net" = mk-extension "ublock-origin";
+  "ff2mpv@yossarian.net" = mk-extension "ff2mpv";
+  "sponsorBlocker@ajay.app" = mk-extension "sponsorblock";
+  "deArrow@ajay.app" = mk-extension "dearrow";
+  "addon@darkreader.org" = mk-extension "darkreader";
   "*" = {
     installation_mode = "blocked";
   };
