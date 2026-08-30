533655a5dae4824ed8b792b7d9bcf84a34218e65
Terrame0 <terrame_0@proton.me>
2026-08-30 21:07:25 +1000

    [WIP]
    [hostname 'tuf-f17']
    
    [gen 50 | ver 26.05.20260825.f4f6986]
    
    [2026-08-30 19:09:08]

--- src/network{modules:system}/networking.nix
+++ src/network{modules:system}/networking.nix
@@ -1,10 +1,39 @@
-{host, ...}: {
+{
+  host,
+  pkgs,
+  ...
+}: {
   networking = {
     hostName = host.name;
     nameservers = ["1.1.1.1" "8.8.8.8" "8.8.4.4"];
     networkmanager = {
       enable = true;
       dns = "none";
+      # -- debounce transient ethernet carrier loss
+      settings."device-ethernet" = {
+        match-device = "type:ethernet";
+        carrier-wait-timeout = 15000;
+      };
+      # -- keep wifi inactive while ethernet is connected
+      dispatcherScripts = [
+        {
+          type = "basic";
+          source = pkgs.writeShellScript "70-wifi-wired-exclusive" ''
+            export LC_ALL=C
+
+            case "$2" in
+              up|down)
+                if ${pkgs.networkmanager}/bin/nmcli -t -f TYPE,STATE device status \
+                  | ${pkgs.gnugrep}/bin/grep -qx 'ethernet:connected'; then
+                  ${pkgs.networkmanager}/bin/nmcli radio wifi off
+                else
+                  ${pkgs.networkmanager}/bin/nmcli radio wifi on
+                fi
+                ;;
+            esac
+          '';
+        }
+      ];
     };
     firewall = {
       trustedInterfaces = ["tun0"];
