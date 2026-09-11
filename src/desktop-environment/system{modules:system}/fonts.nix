72041a75646db538f271eced3b9d158649f4a58a
Terrame0 <terrame_0@proton.me>
2026-09-11 14:33:20 +1000

    [WIP]
    [hostname 'tuf-f17']
    
    [gen 79 | ver 26.05.20260825.f4f6986]
    
    [2026-09-10 22:20:33]

--- src/desktop-environment/system{modules:system}/fonts.nix
+++ src/desktop-environment/system{modules:system}/fonts.nix
@@ -1,12 +1,18 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  inputs,
+  ...
+}: {
   fonts = {
     fontconfig.enable = true;
-    packages = with pkgs; [
+    packages = with pkgs;
+    with inputs.microsoft-fonts.packages.${pkgs.system}; [
       monocraft
       nerd-fonts.jetbrains-mono
       noto-fonts
       noto-fonts-cjk-sans
       noto-fonts-color-emoji
+      ttf-ms-win11-auto
     ];
   };
 }
