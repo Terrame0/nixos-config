e90fcd600b78eed14e97a8178b8364a916e0766b
Terrame0 <terrame_0@proton.me>
2026-09-18 22:23:07 +1000

    [WIP]
    [hostname 'tuf-f17']
    
    [gen 119 | ver 26.05.20260916.4c78701]
    
    [2026-09-18 21:30:00]

--- src/applications/user{modules:user}/vscode/decorators{private}/nix-injection.nix
+++ src/applications/user{modules:user}/vscode/decorators{private}/nix-injection.nix
@@ -5,22 +5,31 @@
     rev = "f367ed0935a21b6850f72dbefcb6550a0fee3bae";
     hash = "sha256-f83lGXuUm8HBVanMOGmkLK8la2V0HIFSQMar/9z2IFA=";
   };
-  additions = builtins.fromJSON ''
-    // syntax: json
-    [
-      { "key": "nushell", "triggers": ["nushell", "nu"], "scope": "source.nushell", "langId": "nushell" },
-      { "key": "rasi", "triggers": ["rasi"], "scope": "source.rasi", "langId": "rasi" }
-    ]
-  '';
+  additions =
+    builtins.fromJSON
+    /*
+    json
+    */
+    ''
+      [
+        { "key": "nushell", "triggers": ["nushell", "nu"], "scope": "source.nushell", "langId": "nushell" },
+        { "key": "rasi", "triggers": ["rasi"], "scope": "source.rasi", "langId": "rasi" }
+      ]
+    '';
   upstream = builtins.fromJSON (builtins.readFile "${src}/languages.json");
   inherit (builtins.fromJSON (builtins.readFile "${src}/package.json")) version;
   keys = map (l: l.key) additions;
   merged = additions ++ builtins.filter (l: !(builtins.elem l.key keys)) upstream;
 in {
   "barsikus007.nix-injection".vsix =
-    pkgs.runCommand "nix-injection-${version}.vsix" {
-      nativeBuildInputs = [pkgs.bun pkgs.zip];
-    } ''
+    pkgs.runCommand "nix-injection-${version}.vsix"
+    {
+      nativeBuildInputs = [
+        pkgs.bun
+        pkgs.zip
+      ];
+    }
+    ''
       cp -r ${src}/. .
       chmod -R u+w .
       cp ${pkgs.writeText "languages.json" (builtins.toJSON merged)} languages.json
