cbe48c756db7ef1c1bacc4f987539acc96cead2a
Terrame0 <terrame_0@proton.me>
2026-09-25 17:31:47 +1000

    [WIP]
    [hostname 'tuf-f17']
    
    [gen 170 | ver 26.05.20260916.4c78701]
    
    [2026-09-25 17:26:32]

--- src/applications/user{modules:user}/prismlauncher.nix
+++ src/applications/user{modules:user}/prismlauncher.nix
@@ -1,13 +1,10 @@
-{pkgs, ...}: let
-  prismlauncher-cuda = pkgs.symlinkJoin {
-    name = "prismlauncher-cuda";
-    paths = [pkgs.prismlauncher];
-    nativeBuildInputs = [pkgs.makeWrapper];
-    postBuild = ''
-      wrapProgram $out/bin/prismlauncher \
-        --prefix LD_LIBRARY_PATH : "${pkgs.lib.makeLibraryPath [pkgs.cudaPackages.cudatoolkit pkgs.cudaPackages.cudnn]}"
-    '';
-  };
-in {
-  home.packages = [prismlauncher-cuda];
+{pkgs, ...}: {
+  home.packages = [
+    (pkgs.prismlauncher.override {
+      additionalLibs = [
+        pkgs.cudaPackages.cudatoolkit
+        pkgs.cudaPackages.cudnn
+      ];
+    })
+  ];
 }
